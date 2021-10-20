d33c073e0e943043838476f5f205291b9b494015
wy <wy@szzxue.com>
2021-10-20 20:08:05 +0800

    fix : 修改注释

--- C#/0326.net基础班学习/0326.net基础班资料合集/0326.Net基础班第12天/第12天老师的练习/0326.Net基础班第十二天/05撞向和拆箱/Program.cs
+++ C#/0326.net基础班学习/0326.net基础班资料合集/0326.Net基础班第12天/第12天老师的练习/0326.Net基础班第十二天/05撞向和拆箱/Program.cs
@@ -5,23 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Diagnostics;
+
 namespace _05撞向和拆箱
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //装箱：将值类型转换为引用类型  
+            //装箱：将值类型转换为引用类型
             //拆箱：将引用类型转换为值类型
-            //值类型:int double decimal enum struct bool char 
+            //值类型:int double decimal enum struct bool char
             //引用类型：数组 集合 自定义类 string object
 
             //int n = 10;
             //object o = n;//值类型---》引用类型  装箱
 
             //double nn = (double)o;//引用类型---》值类型 拆箱
-            ////撞向的时候 使用的什么类型 拆箱的时候也得使用对应的类型
-            //Console.WriteLine(nn);
+            ////装箱的时候 使用的什么类型 拆箱的时候也得使用对应的类型
+            //Console.WriteLine(n);
 
             //ArrayList listArr = new ArrayList();
             //List<int> list = new List<int>();
@@ -38,17 +39,12 @@ namespace _05撞向和拆箱
             //Console.WriteLine(sw.Elapsed);
             //Console.ReadKey();
 
-
-
             //string s = "123";
             //int n = Convert.ToInt32(s);//拆箱？这个地方并没有发生任何的装箱或者拆箱
-
 
             //看两个类型发生装箱或者拆箱，首先我们要观察的是发生类型转换的这个两个类型
             //是否存在继承关系。
 
-
-            
             int n = 10;
             IComparable i = n;//值类型---》引用类型  装箱
         }
